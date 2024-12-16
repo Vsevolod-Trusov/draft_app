51fce0d41564372f7232b3e2edd67704c07e7e3d
Michail Shatilo <michail.shatilo@orangesoft.co>
2024-12-16 13:57:08 +0100

    fix: add module

--- src/app/modules/controllers.module.ts
+++ src/app/modules/controllers.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 
+import { MailModule } from './mail';
 import { UserModule } from './user';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, MailModule],
   exports: [UserModule],
 })
 class ControllersModule {}
