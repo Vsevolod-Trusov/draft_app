50208e119579b3992e9d675b1b61da86994ec00e
Michail Shatilo <michail.shatilo@orangesoft.co>
2024-12-18 12:11:39 +0100

    reafactor: coding

--- src/libs/middleware/logging.middleware.ts
+++ src/libs/middleware/logging.middleware.ts
@@ -1,22 +1,18 @@
-import {
-  Inject,
-  Injectable,
-  LoggerService,
-  NestMiddleware,
-} from "@nestjs/common";
-import { WINSTON_MODULE_NEST_PROVIDER } from "nest-winston";
+import { Inject, Injectable, LoggerService, NestMiddleware } from '@nestjs/common';
 
-import { getLoggingInfo } from "core";
-import { FastifyRequest } from "fastify";
+import { getLoggingInfo } from 'core';
+import { FastifyRequest } from 'fastify';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   constructor(
     @Inject(WINSTON_MODULE_NEST_PROVIDER)
-    private readonly logger: LoggerService
+    private readonly logger: LoggerService,
   ) {}
 
   use(request: FastifyRequest, _, done: () => void) {
-    this.logger.log(getLoggingInfo(request), LoggerMiddleware.name), done();
+    this.logger.log(getLoggingInfo(request), LoggerMiddleware.name);
+    done();
   }
 }
