e57a2d130f8d436e6d0d6b121a5d2db6898dc895
Michail Shatilo <michail.shatilo@orangesoft.co>
2024-12-26 18:40:34 +0100

    fix: upgrade eslint to 9x version and change package json, docker file node version to 20, remove unnecessary packages

--- src/config/env/env-validation.schema.ts
+++ src/config/env/env-validation.schema.ts
@@ -1,7 +1,6 @@
 import * as Joi from 'joi';
 
 export const envFileValidationSchema = Joi.object({
-  NODE_ENV: Joi.string().required(),
   BACKEND_PORT: Joi.number().required(),
   BACKEND_ADDRESS: Joi.string().required(),
   DATABASE_CONNECTION_STRING: Joi.string().required(),
